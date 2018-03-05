f9ffb3c9720c4ffefbfe259659245d091606c2f6
Dean van Niekerk <deanvniekerk@gmail.com>
2018-03-05 15:44:53 +0200

    Added bundle to git

--- bin/www.js
+++ bin/www.js
@@ -1,6 +1,6 @@
 var app = require('../server/server');
 
-var port = process.env.port || 1337
+var port = process.env.PORT || 1337
 
 app.listen(port, function() {
  console.log('running at localhost: ' + port);
